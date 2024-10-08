57699d5fc7edded2de67671521b33718671bbea3
Arya Akmal <60330179+aryaakmal@users.noreply.github.com>
2024-10-08 14:38:48 -0400

    Update chatgui.h

--- src/chatgui.h
+++ src/chatgui.h
@@ -2,6 +2,7 @@
 #define CHATGUI_H_
 
 #include <wx/wx.h>
+#include <memory>
 
 class ChatLogic; // forward declaration
 
@@ -16,7 +17,10 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+   //ChatLogic *_chatLogic;
+   //std::unique_ptr<ChatLogic> _chatLogic(new ChatLogic);
+     std::unique_ptr<ChatLogic> _chatLogic;
+   // _chatLogic = std::make_unique<ChatLogic>();  
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +31,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic *GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
